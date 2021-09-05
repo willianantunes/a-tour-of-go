7e8be5816d10bb3fdd59dacabec68567c2aac539
Willian Antunes <willian.lima.antunes@gmail.com>
2021-09-05 12:13:19 -0300

    feat: add tests in regard to maps

---
+++ 4_structs_slices_maps/maps.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+type Vertex struct {
+	Lat, Long float64
+}
+
+// A map maps keys to values.
+// The zero value of a map is nil. A nil map has no keys, nor can keys be added.
+var m map[string]Vertex
+
+func main() {
+
+	// The make function returns a map of the given type, initialized and ready for use.
+	m = make(map[string]Vertex)
+	m["Bell Labs"] = Vertex{
+		40.68433, -74.39967,
+	}
+	fmt.Println(m["Bell Labs"]) // {40.68433 -74.39967}
+}
